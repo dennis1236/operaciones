be95cd124379fbefdaa9b54b25d826ae522a93ae
dennis1236 <dennisecheverri46@gmail.com>
2023-11-16 10:20:10 -0500

    Vamos bien

--- suma.js
+++ suma.js
@@ -1,1 +1,14 @@
-#Aqui viene java Scritp 
+function Suma() {
+
+    let numero_1 = parseFloat(document.getElementById('num_1').value)
+
+    let numero_2 = parseFloat(document.getElementById('num_2').value)
+
+    if (!isNaN(numero_1)) && !isNaN(numero_2) {
+
+        let suma = numero_1 + numero_2
+        
+
+    }
+
+}
