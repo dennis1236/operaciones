74f3446151ba1bd104b2d5443618c3cb4ec199d3
dennis1236 <dennisecheverri46@gmail.com>
2023-11-16 10:34:22 -0500

    suma realizada

--- suma.js
+++ suma.js
@@ -1,14 +1,17 @@
-function Suma() {
+/* function Suma() {
 
-    let numero_1 = parseFloat(document.getElementById('num_1').value)
+    var numero_1 = parseFloat(document.getElementById('num_1').value);
 
-    let numero_2 = parseFloat(document.getElementById('num_2').value)
+    var numero_2 = parseFloat(document.getElementById('num_2').value);
 
-    if (!isNaN(numero_1)) && !isNaN(numero_2) {
+    if (!isNaN(numero_1) && !isNaN(numero_2)) {
 
-        let suma = numero_1 + numero_2
-        
+        var suma = numero_1 + numero_2;
 
+        document.getElementById('mensaje').innerHTML = "La suma de los dos numeros es: " + suma;
+
+    } else {
+        document.getElementById('mensaje').innerHTML = "Digitacion no valida";
     }
 
-}
+} */
